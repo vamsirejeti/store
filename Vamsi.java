24d9d3757053c837b174cb5717b7c1fb12456d7c
vamsirejeti <92706480+vamsirejeti@users.noreply.github.com>
2021-10-18 10:44:16 +0530

    new class added

--- Vamsi.java
+++ Vamsi.java
@@ -1,7 +1,21 @@
+class Student
+{
+  int rollno;
+  String name;
+  public void display()
+  {
+    rollno = 1;
+    name = "Vamsi";
+   
+  }
+}
 public class Vamsi
 {
-  public static void man(String[] args)
+  public static void main(String[] args)
   {
-    system.out.println("Hello");
+    System.out.println("Hello");
+    Student obj = new Student();
+    obj.display();
+    System.out.println(obj.rollno);
   }
 }
